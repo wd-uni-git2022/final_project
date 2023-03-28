9887cbbcd78a420b971f2c1d0832eabd75569e9e
wd-uni-git2022 <104198435+wd-uni-git2022@users.noreply.github.com>
2023-03-28 21:29:08 +0200

    Add user entity

---
+++ backend/src/main/java/htw/webdev/max/model/User.java
@@ -0,0 +1,30 @@
+package htw.webdev.max.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private Long userId;
+
+    private String username;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
