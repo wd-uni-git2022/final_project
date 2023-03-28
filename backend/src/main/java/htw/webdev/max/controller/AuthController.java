e82e278f09e8edb52f80838680dbeef47d550ae9
wd-uni-git2022 <104198435+wd-uni-git2022@users.noreply.github.com>
2023-03-28 19:14:16 +0200

    Add auth and quiz controller

--- backend/src/main/java/htw/webdev/max/controller/AuthController.java
+++ backend/src/main/java/htw/webdev/max/controller/AuthController.java
@@ -1,2 +1,31 @@
-package htw.webdev.max.controller;public class AuthController {
+package htw.webdev.max.controller;
+
+import io.smallrye.jwt.build.Jwt;
+import org.eclipse.microprofile.jwt.Claims;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Path("/api/auth")
+public class AuthController {
+
+    @POST
+    @Path("login")
+    public String login() {
+        String token = Jwt.issuer("http://localhost/issuer")
+                .upn("testuser@example.org")
+                .groups(new HashSet<>(Arrays.asList("User")))
+                .claim(Claims.preferred_username.name(), "testuser")
+                .sign();
+
+        return token;
+    }
+
+    @POST
+    @Path("register")
+    public String register() {
+        return "register test";
+    }
 }
--- backend/src/main/java/htw/webdev/max/controller/QuizController.java
+++ backend/src/main/java/htw/webdev/max/controller/QuizController.java
@@ -1,2 +1,21 @@
-package htw.webdev.max.controller;public class QuizController {
+package htw.webdev.max.controller;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/api/quiz")
+public class QuizController {
+
+    @GET
+    public String getQuizzes(){
+        return "test response";
+    }
+
+    @POST
+    @RolesAllowed({"User", "Admin"})
+    public String addQuiz(){
+        return "test response: added quiz";
+    }
 }
