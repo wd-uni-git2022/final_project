40cc4f25ee2b16c3540f47b9644cd0e0f9c5c551
wd-uni-git2022 <104198435+wd-uni-git2022@users.noreply.github.com>
2023-03-31 18:57:02 +0200

    Add quiz page

---
+++ frontend/src/pages/QuizPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+
+  </q-page>
+</template>
+
+<script setup>
+import {onMounted} from "vue";
+import {api} from "boot/axios";
+
+onMounted(() => {
+  api.get("api/quiz").then((response) => {
+    console.log(response.data);
+  }).catch((error) => {
+    console.log(error);
+  })
+})
+</script>
