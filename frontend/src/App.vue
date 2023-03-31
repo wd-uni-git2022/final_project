2fb71bc411c52c60f26f76464df3ab73a1a18680
wd-uni-git2022 <104198435+wd-uni-git2022@users.noreply.github.com>
2023-03-31 20:09:58 +0200

    Load token from local storage if it exists

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <router-view />
+  <router-view/>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
+import {api} from "boot/axios";
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  beforeMount() {
+    console.log("beforeMount App.vue");
+    let token = localStorage.getItem("token");
+    if (token && token !== "") {
+      console.log("token found in localStorage");
+      api.defaults.headers.common["Authorization"] = "Bearer " + token;
+    }
+
+  }
+
 })
+
+
 </script>
