534829d9dea5eb48a590c46291543d8d4d0201c1
wd-uni-git2022 <104198435+wd-uni-git2022@users.noreply.github.com>
2023-03-31 15:03:39 +0200

    Connect frontend registration with backend auth controller

--- backend/src/main/java/htw/webdev/max/controller/AuthController.java
+++ backend/src/main/java/htw/webdev/max/controller/AuthController.java
@@ -1,7 +1,9 @@
 package htw.webdev.max.controller;
 
+import htw.webdev.max.model.AppUser;
 import io.smallrye.jwt.build.Jwt;
 import org.eclipse.microprofile.jwt.Claims;
+import org.jboss.logging.Logger;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -11,6 +13,9 @@ import java.util.HashSet;
 
 @Path("/api/auth")
 public class AuthController {
+
+    private static final Logger LOG = Logger.getLogger(AuthController.class);
+
 
     @POST
     @Path("login")
@@ -27,7 +32,10 @@ public class AuthController {
 
     @POST
     @Path("register")
-    public String register() {
+    public String register(AppUser appUser) {
+
+        LOG.info("appUser: " + appUser.getUsername());
+
         return "register test";
     }
 }
--- frontend/src/pages/RegisterPage.vue
+++ frontend/src/pages/RegisterPage.vue
@@ -19,18 +19,34 @@
         filled
         v-model="password"
         label="Password"
-        type="password"
+        :type="isPwd ? 'password' : 'text'"
         lazy-rules
         :rules="[ val => val && val.length >= 10 || 'Please enter a password with at least 10 characters']"
-      />
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
       <q-input
         filled
         v-model="passwordConfirm"
         label="Confirm password"
-        type="password"
+        :type="isPwd ? 'password' : 'text'"
         lazy-rules
         :rules="[ val => val && val.length >= 10 && val === password || 'Password doesn\'t match']"
-      />
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd2 ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd2 = !isPwd2"
+          />
+        </template>
+      </q-input>
 
       <div>
         <q-btn label="Submit" type="submit" color="primary"/>
@@ -42,12 +58,29 @@
 <script setup>
 
 import {ref} from "vue";
+import axios from "axios";
+
 
 const username = ref("")
 const email = ref("")
 const password = ref("")
 const passwordConfirm = ref("")
-function onSubmit(){
+
+const isPwd = ref(true)
+const isPwd2 = ref(true)
+
+function onSubmit() {
   console.log("form submit");
+  axios.post("/api/auth/register",
+    {
+      "username": username.value,
+      "email": email.value,
+      "password": password.value
+    }).then((response) => {
+      console.log("response: " + response.data);
+  }).catch((error) => {
+    console.log(error);
+  })
+
 }
 </script>
